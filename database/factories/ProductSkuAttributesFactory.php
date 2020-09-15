3b36cf3c7364933577601c25476f3bf2794b8047
vagrant <vagrant@homestead>
2020-09-15 16:00:16 +0000

    商品模型

---
+++ database/factories/ProductSkuAttributesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ProductSkuAttributes;
+use Faker\Generator as Faker;
+
+$factory->define(ProductSkuAttributes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ProdutSkuAttributesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ProdutSkuAttributes;
+use Faker\Generator as Faker;
+
+$factory->define(ProdutSkuAttributes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
