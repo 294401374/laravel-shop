da1b00460768f1167fd1a7ea1e6bc22997e1db2f
baotong <294401374@qq.com>
2020-09-20 22:22:47 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,9 +7,32 @@ use App\Models\Product;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
-        return view('products.index', compact('products'));
+        $buider = Product::query()->where('on_sale', true);
+        if ($search = $request->input('search', '')){
+            $like = '%' . $search . '%';
+            $buider->where(function ($query) use ($like){
+                $query->where('title', 'like' , $like)
+                      ->orWhere('description', 'like', $like)
+                      ->orWhereHas('skus', function($query) use ($like){
+                         $query->where('title', 'like', $like)
+                               ->orWhere('description', 'like', $like);
+                      });
+            });
+        }
+        if ($order = $request->input('order', '')){
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $buider->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $buider->paginate(16);
+        $filters = [
+            'search' => $search,
+            'order' => $order,
+        ];
+        return view('products.index', compact('products', 'filters'));
     }
 }
